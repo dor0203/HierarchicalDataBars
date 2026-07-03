230f507e898871fb32b8c99057cd3bf455f5c103
dor <dor>
2026-07-03 22:28:24 +0300

    cleaned uneccesary folders

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var hierarchicalDataBars: IVisualPlugin;
-export default hierarchicalDataBars;
